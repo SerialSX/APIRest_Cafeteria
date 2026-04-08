194c29a49a76fb4038909da21f110189225841dd
João Arthur <joaoarthurce@gmail.com>
2026-04-08 15:11:49 -0300

    fix: porta dinamica para o Render

--- app.js
+++ app.js
@@ -10,4 +10,5 @@ app.use('/produtos', productRoutes);
 app.use('/funcionarios', employeeRoutes);
 app.use('/pedidos', orderRoutes);
 
-app.listen(3000, () => console.log("Servidor rodando na porta 3000 ☕"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor a correr na porta ${PORT} ☕`));
